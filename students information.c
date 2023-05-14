14e1569876939b7c8068bfc74626d9c951917f0e
Bhavesh5997 <cd.bhavesh0987654321@gmail.com>
2023-05-14 16:48:06 +0530

    I changed file because i got error on students percentage.This program also arrange students result on descending order

--- students information.c
+++ students information.c
@@ -3,7 +3,7 @@ struct student
 {
 int rollno;
 char name[20];
-int m1,m2,m3,m4,m5;
+float m1,m2,m3,m4,m5;
 float percent;
 };
 void getdetail(struct student *);
@@ -27,15 +27,15 @@ scanf("%d",&ptr->rollno);
 printf("\n enter the name \n");
 scanf("%s",ptr->name);
 printf("\n enter the sub1 mark=");
-scanf("%d",&ptr->m1);
+scanf("%f",&ptr->m1);
 printf("\n enter the sub2 mark=");
-scanf("%d",&ptr->m2);
+scanf("%f",&ptr->m2);
 printf("\n enter the sub3 mark=");
-scanf("%d",&ptr->m3);
+scanf("%f",&ptr->m3);
 printf("\n enter the sub4 mark=");
-scanf("%d",&ptr->m4);
+scanf("%f",&ptr->m4);
 printf("\n enter the sub5 mark=");
-scanf("%d",&ptr->m5);
+scanf("%f",&ptr->m5);
 ptr->percent=(ptr->m1+ptr->m2+ptr->m3+ptr->m4+ptr->m5)/5;
 ptr++;
 }
@@ -62,11 +62,11 @@ for(i=0;i<10;i++)
 {
 printf("\n rollno=%d",(ptr+i)->rollno);
 printf("\n name=%s",(ptr+i)->name);
-printf("\n sub1 mark=%d",(ptr+i)->m1);
-printf("\n sub2 mark=%d",(ptr+i)->m2);
-printf("\n sub3 mark=%d",(ptr+i)->m3);
-printf("\n sub4 mark=%d",(ptr+i)->m4);
-printf("\n sub5 mark=%d",(ptr+i)->m5);
+printf("\n sub1 mark=%f",(ptr+i)->m1);
+printf("\n sub2 mark=%f",(ptr+i)->m2);
+printf("\n sub3 mark=%f",(ptr+i)->m3);
+printf("\n sub4 mark=%f",(ptr+i)->m4);
+printf("\n sub5 mark=%f",(ptr+i)->m5);
 printf("\n percent=%f\n",(ptr+i)->percent);
 }
 }
